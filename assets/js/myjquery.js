c2bfc925ae32db26ab50ac2186c1d72b0db463c3
user <jonathan.caron04@gmail.com>
2017-10-05 09:23:48 +0200

    cv 1.0

---
+++ assets/js/myjquery.js
@@ -0,0 +1,9 @@
+$(function(){
+
+        /*$(".tab").css('display:block;');
+
+        $(".open-tab").click(function(){
+        $(".tab").hide();
+        $('[data-tab ="' + $(this).data('tab') + '"]').show(1000);
+    });*/
+});
---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" description="CV de Jonathan Caron, jeune étudiant de 20 ans.">
+    <title>CV - Jonathan Caron</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/styles.css">
+
+  </head>
+
+
+  <body>
+    <?php include('./assets/requires/dblog.php'); ?>
+    <div class="main">
+
+
+
+
+
+      <div id="navbar">
+        <p>
+          <br/>
+          <span class="texteTitre">Jonathan Caron</span>
+          <br/>
+          <span class="texteSousTitre">Développeur Web</span>
+
+        </p>
+      </div>
+
+
+
+      <div id="block1">
+        <img src="./assets/images/codinglogo.png" width="120px" height="120px" style="margin-top:-80px; margin-left:20%;display:flow;">
+      <p style="margin-top:-1px; margin-left:28%; margin-right:18%; text-align:justify;">
+        Je me nomme Jonathan Caron et je suis un jeune web developpeur autodidacte. Je suis passionné par l'informatique depuis tout petit et je m'intéresse à toutes sortes de technologies dans le but d'apprendre en continue.
+<br/><br/>Je suis diplômé du CESS, de la gestion et de la bureautique. J'ai eu une bonne expérience dans l'administration-bureautique en travaillant pour Belfius Waterloo SCRL mais également dans divers domaine tels que le marketing et la communication avec la clientèle.</p>
+
+      </div>
+    </br>
+
+      <a style="margin-left:21%;" class="classLi1" href="#competences"><div class='open-tab'data-tab='a'>Compétences</div></a>
+      <a class="classLi1" href="#experience"><div class='open-tab' data-tab='b'>Expériences</div></a>
+      <a class="classLi1" href="#etudes"><div class='open-tab' data-tab='c'>Etudes</div></a>
+
+    <a class="classLi1" href="#contact">Contact</a>
+    <div class="texteRight"><!-- add style="display:none; for add myjequery -->
+      <div class='tab' data-tab='a'>
+        <h3 id="competences"><span class="categorie">Compétences</span></h3>
+        <p>
+
+          <div class="blockSkill">
+            <img src="./assets/images/html.png" width="30px" height="25px">
+            <span class="titleSkill">HTML</span>
+            <progress value="60" max="100" class="progressBar">
+            </progress>
+          </div>
+
+          <div class="blockSkill">
+            <img src="./assets/images/css3.png" width="30px" height="25px">
+            <span class="titleSkill">CSS</span>
+            <progress value="60" max="100" class="progressBar">
+            </progress>
+          </div>
+
+          <div class="blockSkill">
+            <img src="./assets/images/php.png" width="30px" height="25px">
+            <span class="titleSkill">PHP</span>
+            <progress value="40" max="100" class="progressBar">
+            </progress>
+          </div>
+
+          <div class="blockSkill">
+            <img src="./assets/images/mysql.png" width="30px" height="25px">
+            <span class="titleSkill">MYSQL</span>
+            <progress value="20" max="100" class="progressBar">
+            </progress>
+          </div>
+          <br>
+          <div class="blockSkill">
+            <img src="./assets/images/js.png" width="30px" height="25px">
+            <span class="titleSkill">Javascript</span>
+            <progress value="22" max="100" class="progressBar">
+            </progress>
+          </div>
+
+          <div class="blockSkill">
+            <img src="./assets/images/js.png" width="30px" height="25px">
+            <span class="titleSkill">nodeJS</span>
+            <progress value="5" max="100" class="progressBar">
+            </progress>
+          </div>
+
+          <div class="blockSkill">
+            <img src="./assets/images/js.png" width="30px" height="25px">
+            <span class="titleSkill">jQuery</span>
+            <progress value="10" max="100" class="progressBar">
+            </progress>
+          </div>
+
+          <div class="blockSkill">
+            <img src="./assets/images/bootstrap.png" width="30px" height="25px">
+            <span class="titleSkill">Bootstrap</span>
+            <progress value="5" max="100" class="progressBar">
+            </progress>
+          </div>
+
+
+          <br/>
+          <br/>
+        </div><!-- add style="display:none; for add myjequery -->
+        <div class='tab' data-tab='b'>
+          <h3 id="experiences"><span class="categorie">Expériences</span></h3>
+
+          <img src="./assets/images/puce.png" width="30px" height="25px">
+          <span class="textePuce">2017 - aujourd'hui : gestionnaire des réseaux sociaux pour Belfius Waterloo SCRL.</span>
+          </br>
+
+          <img src="./assets/images/puce.png" width="30px" height="25px">
+          <span class="textePuce">2017 : Formation 'web development' chez BeCode (6 mois).</span>
+          </br>
+
+          <img src="./assets/images/puce.png" width="30px" height="25px">
+          <span class="textePuce">2017 : Stagiaire chez Belfius Waterloo SCRL (6 semaines)</span>
+          </br>
+
+          <img src="./assets/images/puce.png" width="30px" height="25px">
+          <span class="textePuce">2017 : Etudiant chez Belfius Waterloo SCRL (1 mois).</span>
+          </br>
+
+          <img src="./assets/images/puce.png" width="30px" height="25px">
+          <span class="textePuce">2016 : Stagiaire chez Basic-Fit (4 semaines).</span>
+          </br>
+
+          <br/>
+          <br/>
+        </div><!-- add style="display:none; for add myjequery -->
+        <div class='tab' data-tab='c'>
+
+          <h3 id="etudes"><span class="categorie">Etudes</span></h3>
+          <img src="./assets/images/puceetudes.png" width="30px" height="25px">
+          <span class="textePuce">2015-2017 : 5e et 6e technique gestion – technicien de bureau à l'Athénée Royal de Braine l'Alleud.</span>
+          </br>
+          <img src="./assets/images/puceetudes.png" width="30px" height="25px">
+          <span class="textePuce">2012-2015 : 3e et 4e technique gestion à l'Athénée Royal de Braine l'Alleud.</span>
+          </br>
+          <img src="./assets/images/puceetudes.png" width="30px" height="25px">
+          <span class="textePuce">2009-2012 : 1e et 2e général au Collège du Sacré-Cœur de Ganshoren. </span>
+          </br>
+
+
+          <br/>
+          <br/>
+
+
+        </p>
+      </div>
+    </div>
+    <div id="block1">
+      <div class="texteRight">
+        <br>
+        <h3 id="contact"><span class="categorie">Contact</span></h3>
+
+          <?php
+          if(!empty($_POST)){
+            ?>
+            <p style="color:green">Votre message a bien été envoyé !</p>
+            <?php
+            $pPrenom= $_POST["prenom"];
+            $pNom= $_POST["nom"];
+            $pEmail= $_POST["email"];
+            $pMessage= $_POST["message"];
+            $bdd->exec("INSERT INTO contact (prenom, nom, email, message) VALUES ('$pPrenom', '$pNom', '$pEmail', '$pMessage')");
+            ?>
+            <form class="" action="index.php" method="post">
+              Prénom :<br>
+              <input type="text" name="prenom" value="<?php echo $pPrenom; ?>"  disabled><br><br>
+              Nom :<br>
+              <input type="text" name="nom" value="<?php echo $pNom; ?>" disabled><br><br>
+              Adresse mail :<br>
+              <input type="text" name="email" value="<?php echo $pEmail; ?>" disabled><br><br>
+              Votre demande :<br>
+              <textarea name="message" rows="8" cols="80" disabled><?php echo $pMessage; ?></textarea><br>
+            </form>
+            <form action="index.php#contact" method="post">
+              <input type="submit" value="Retour">
+            </form>
+          <?php
+          }
+          else{
+          ?>
+            <form class="" action="index.php#contact" method="post">
+              Prénom :<br>
+              <input type="text" name="prenom" value=""><br><br>
+              Nom :<br>
+              <input type="text" name="nom" value=""><br><br>
+              Adresse mail :<br>
+              <input type="text" name="email" value=""><br><br>
+              Votre demande :<br>
+              <textarea name="message" rows="8" cols="80"></textarea><br>
+              <input type="submit" value="Envoyer">
+            </form>
+          <?php } ?>
+
+        </div>
+
+    </div>
+
+
+    </div>
+    <script src="./assets/js/jquery-3.2.1.min.js"></script>
+    <script src="./assets/js/myjquery.js"></script>
+  </body>
+
+</html>
